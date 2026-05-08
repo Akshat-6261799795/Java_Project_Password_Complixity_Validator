4c510fa2b3032f61a74a5ee6048e546d949d46ee
Akshat Rahangdale <akshatrahangdale31@gmail.com>
2026-05-08 18:47:48 +0530

    Prototypr complete of the project

--- Password_compixity_Validator.java
+++ Password_compixity_Validator.java
@@ -1,4 +1,5 @@
 class UserAccount{
+
     private String username;
     private String password;
 
@@ -16,21 +17,49 @@ class UserAccount{
     }
 }
 
-class SecutiryValidator{
+class SecurityValidator{
     public boolean isValid(UserAccount user){
 
         String password = user.getPassword();
 
-        if (password.length()<8){
+        if (password.length() < 8){
             return false;
-
         }
 
-    }
+        boolean hasDigit = false;
+        boolean hasUpper = false;
 
+        for (int i = 0; i < password.length(); i++){
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)){
+                hasDigit = true;
+            }
+            if (Character.isUpperCase(ch)){
+                hasUpper = true;
+            }
+        }
+
+        if (hasDigit && hasUpper){
+            return true;
+        }
+        return false;
+
+    }
 }
+
 public class Password_compixity_Validator{
     public static void main(String[] args){
         
+        UserAccount user = new UserAccount("Ajay", "1324567890");
+
+        SecurityValidator validator = new SecurityValidator();
+        boolean isValid = validator.isValid(user);
+
+        if (isValid){
+            System.out.println("Password is valid.");
+        }
+        else{
+            System.out.println("Password is invalid.");
+        }
     }
 }
